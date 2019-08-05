10eb7722af25b5b930bdb54c1b34a4ab8e7b1f2b
JCRN <clemons.jason@gmail.com>
2019-08-05 13:25:29 -0700

    WIP: completed component structures for each component, wiring up functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TodoList from './components/TodoComponents/TodoList'
+import TodoForm from './components/TodoComponents/TodoForm'
 
 const tasks = [
   {
@@ -24,17 +25,48 @@ class App extends React.Component {
       tasks
     }
   }
+
+  toggleTask = id => {
+    console.log(id)
+    this.setState({
+      tasks: this.state.tasks.map(task => {
+        if (task.id === id) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        } else {
+          return task
+        }
+      })
+    })
+  }
+
+  addTask = taskName => {
+    const newTask = {
+      task: taskName,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    })
+  }
+
+  clearCompleted = () => {
+    this.setState({
+      tasks: this.state.tasks.filter(task => !task.completed)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="header">
           <h2>Welcome to your Todo App!</h2>
+          <TodoForm addTask={this.addTask} />
         </div>
-        <TodoList
-          tasks={this.state.tasks}
-          clearCompleted={null}
-          toggleTask={null}
-        />
+        <TodoList tasks={this.state.tasks} toggleTask={this.toggleTask} />
       </div>
     )
   }
