94f5cfc68f15b43433558a04c6d5e87ccdbea280
JCRN <clemons.jason@gmail.com>
2019-08-13 20:15:30 -0700

    initialized

---
+++ src/reducers/reducer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const reducer = (state, action) {
+    let newState = {};
+    return newState
+}
+
+
+export const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: Date.now()
+}
